0b05ae3cb4bbc1724259368add4eb9e0054659fe
Daniel Kempner <daniel.kempner@instacart.com>
2022-09-15 13:16:25 -0700

    multiple components, ssr off

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,28 @@
 import type { NextPage } from "next";
 import { useQuery } from "@apollo/client";
 import { QUERY, Job } from "../lib/apollo";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) return null;
+  return (
+    <>
+      <JobsCount />
+      <JobsCount />
+      <JobsCount />
+      <JobsCount />
+      <JobsCount />
+    </>
+  );
+};
+
+function JobsCount() {
   const { data, loading, error } = useQuery<{ jobs: Job[] }>(QUERY);
   return <pre>{data?.jobs?.length}</pre>;
-};
+}
 
 export default Home;
