581969d627c2564fe0015e50220923de5e2c6b14
Daniel Kempner <daniel.kempner@instacart.com>
2022-09-15 13:02:44 -0700

    rendering

---
+++ lib/apollo.ts
@@ -0,0 +1,36 @@
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const isServer = typeof window === "undefined";
+// @ts-ignore
+const windowApolloState = !isServer && window.__NEXT_DATA__.apolloState;
+
+let CLIENT: any;
+
+export function getApolloClient(forceNew?: boolean) {
+  if (!CLIENT || forceNew) {
+    CLIENT = new ApolloClient({
+      ssrMode: isServer,
+      uri: "https://api.graphql.jobs/",
+      cache: new InMemoryCache().restore(windowApolloState || {}),
+    });
+  }
+
+  return CLIENT;
+}
+
+export type Job = {
+  __typename: string;
+  id: string;
+  title: string;
+  postedAt: Date;
+};
+
+export const QUERY = gql`
+  query Jobs {
+    jobs {
+      id
+      title
+      postedAt
+    }
+  }
+`;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import { getApolloClient } from "../lib/apollo";
+
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = getApolloClient();
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
